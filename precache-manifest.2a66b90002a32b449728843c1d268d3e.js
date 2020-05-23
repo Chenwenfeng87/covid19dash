511678fe49ff767990dd68d6e8837b146357814a
Tajudin <wenfeng.chen@hand-sg.com>
2020-05-23 13:22:37 +0800

    Updates

---
+++ precache-manifest.2a66b90002a32b449728843c1d268d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5105ad2169c374db05c50b6864e269",
+    "url": "/covid19dash/index.html"
+  },
+  {
+    "revision": "b677c074364dcbbe6d65",
+    "url": "/covid19dash/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11ad493439ecc0e007a0",
+    "url": "/covid19dash/static/js/2.32e11c71.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19dash/static/js/2.32e11c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b677c074364dcbbe6d65",
+    "url": "/covid19dash/static/js/main.f38e0f0b.chunk.js"
+  },
+  {
+    "revision": "1face6a49e7a672b5255",
+    "url": "/covid19dash/static/js/runtime-main.abce201b.js"
+  }
+]);
